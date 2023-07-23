f044c1cef2b9356393f5f34c888b371c78999b7e
galieiny <140314270+galieiny@users.noreply.github.com>
2023-07-23 13:51:39 +0300

    camel case

--- factorial.py
+++ factorial.py
@@ -1,15 +1,15 @@
-def factorial_iterative(n):
+def FactorialIterative(n):
   ans=1
   for i in range(2,n+1):
     ans*=i
   return ans
 
-def factorial_recursive(n):
+def FactorialRecursive(n):
   if n==1 or n==0:
     return 1
   return n*factorial_recursive(n-1)
 
-def is_prime(n):
+def IsPrime(n):
   for i in range(2,n):
     if n%i==0:
       return False
